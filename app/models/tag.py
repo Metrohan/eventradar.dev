3b53503d588f0313556eaaf8441e818dbb95326e
Metrohan <metehangnn@outlook.com>
2026-05-25 18:54:05 +0300

    fix: remove unused relationship import from tag.py

--- app/models/tag.py
+++ app/models/tag.py
@@ -1,5 +1,4 @@
 from sqlalchemy import Column, Integer, String, Table, ForeignKey, PrimaryKeyConstraint
-from sqlalchemy.orm import relationship
 from ..core.database import Base
 
 event_tags = Table(
